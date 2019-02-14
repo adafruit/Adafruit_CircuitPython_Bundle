f6d11782126762945d99f4eabbb5716986e6f8d4
sommersoft <sommersoft@gmail.com>
2019-02-13 19:48:44 -0600

    remove bus_device, register, and CP from intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -23,11 +23,11 @@ extensions = [
 # autodoc_mock_imports = ["digitalio", "busio"]
 
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),
-                       'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),
+intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None)}
+                       """'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),
                        'Register': ('https://circuitpython.readthedocs.io/projects/register/en/latest/', None),
                        'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)
-                       }
+                       }"""
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
