821fd4716914f3e984a6d6cabaf898cd3e2a2238
Dan Halbert <halbert@halwitz.org>
2019-07-10 17:09:49 -0400

    Exclude alpha and beta releases

--- update-submodules.sh
+++ update-submodules.sh
@@ -27,4 +27,8 @@
 git submodule init
 git submodule update
 git submodule foreach git fetch
-git submodule foreach "tag=\$(git rev-list --tags --max-count=1); git checkout -q \$tag"
+
+# Regular release tags are 'x.x.x'. Exclude tags that are alpha or beta releases
+# They will contain a '-' in the tag, such as '3.0.0-beta.5'
+# --exclude must be before --tags.
+git submodule foreach "tag=\$(git rev-list --exclude='*-*' --tags --max-count=1); git checkout -q \$tag"
